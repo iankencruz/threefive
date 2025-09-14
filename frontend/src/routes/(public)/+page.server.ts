752d8e24b1f4b8e0852352316ea325a02c94693c
iankendoit <iankencruz@gmail.com>
2025-09-14 23:33:35 +1000

    fix server calls

--- frontend/src/routes/(public)/+page.server.ts
+++ frontend/src/routes/(public)/+page.server.ts
@@ -1,19 +1,19 @@
-export const ssr = true
+export const ssr = true;
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-  const res = await fetch('http://localhost:8080/api/v1/home');
-  const json = await res.json();
+	const res = await fetch('http://localhost:8080/api/v1/home');
+	const json = await res.json();
 
+	const homeGallery = json.data.galleries.find(
+		(item: any) => item.gallery.slug === 'home-hero-gallery'
+	);
+	console.log('Found homeGallery:', homeGallery);
 
-
-  const homeGallery = json.data.galleries.find((item: any) => item.gallery.slug === 'home-hero-gallery');
-  console.log('Found homeGallery:', homeGallery);
-
-  return {
-    Page: json.data.page,
-    Content: json.data.page.content,
-    HomeGallery: homeGallery,
-    Galleries: json.data.galleries
-  };
+	return {
+		Page: json.data.page,
+		Content: json.data.page.content,
+		HomeGallery: homeGallery,
+		Galleries: json.data.galleries
+	};
 };
